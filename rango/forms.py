bc8c1be15bd9d73ca6f1b4c6ccd6c8f7957729fd
jwh5566 <jwh5566@foxmail.com>
2017-08-14 16:41:29 +0800

    seventh commit

--- rango/forms.py
+++ rango/forms.py
@@ -15,8 +15,14 @@ class CategoryForm(forms.ModelForm):
 
 
 class PageForm(forms.ModelForm):
-    model = Page
-    exclude = ('category',)
+    title = forms.CharField(max_length=128,
+                            help_text="Please enter the title of the page.")
+    url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    class Meta:
+        model = Page
+        exclude = ('category', )
 
     def clean(self):
         cleaned_data = self.cleaned_data
